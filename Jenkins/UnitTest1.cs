c7b76d39933e26e1f0aba9197afa32be5f1a296a
bebebe <hahaha@gmail.com>
2019-11-18 15:49:35 +0200

    bug fixed

--- Jenkins/UnitTest1.cs
+++ Jenkins/UnitTest1.cs
@@ -13,8 +13,10 @@ namespace Jenkins
         public void TestMethod1()
         {
             Console.WriteLine("start test");
-            int i = 0;
+            //int i = 0;
+            double i = 0;
             i = 1 / i;
+            Console.WriteLine("i = " + i);
             Console.WriteLine("done test");
         }
     }
