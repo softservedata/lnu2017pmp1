2e39e7a9a8602e1d8e86081832de5c035f4e300e
bebebe <hahaha@gmail.com>
2019-11-29 11:29:38 +0200

    Add loggers

--- Jenkins/UnitTest1.cs
+++ Jenkins/UnitTest1.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 //using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NUnit.Framework;
+//using log4net;
+//using log4net.Config;
+using NLog;
 
 namespace Jenkins
 {
@@ -8,13 +11,27 @@ namespace Jenkins
     [TestFixture]
     public class UnitTest1
     {
+        //public static ILog log = LogManager.GetLogger(typeof(UnitTest1)); // for Log4net
+        //public static Logger log = LogManager.GetCurrentClassLogger(); // for NLog
+        public static Logger log = LogManager.GetLogger("rolling0"); // for NLog
+
         //[TestMethod]
         [Test]
         public void TestMethod1()
         {
+            //BasicConfigurator.Configure();
+            //XmlConfigurator.Configure();
+            //
+            log.Trace("NLOG: Trace Level test");
+            log.Debug("2*Debug Level test");
+            log.Info("2*Info Level");
+            log.Warn("2*Warn Level");
+            log.Error("2*Error Level test");
+            log.Fatal("2*Fatal Level");
+            //
             Console.WriteLine("start test");
             //int i = 0;
-            double i = 0;
+            double i = 10;
             i = 1 / i;
             Console.WriteLine("i = " + i);
             Console.WriteLine("done test");
